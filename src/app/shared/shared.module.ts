7bd5b5679b471c3bd3f415f59c79d713168405e9
ankurkejriwal <kejriwal99@gmail.com>
2021-07-07 21:55:33 -0400

    Header and footer work

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import {MatDividerModule} from '@angular/material/divider'
+import{MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from '@angular/material/icon'
+import{MatButtonModule} from '@angular/material/button'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 
 
@@ -15,7 +22,14 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule
   ],
   exports: [
     HeaderComponent,
