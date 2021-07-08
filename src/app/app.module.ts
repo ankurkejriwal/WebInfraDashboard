6258ecf701ad10b04258028cb56138f90e3a0b67
ankurkejriwal <kejriwal99@gmail.com>
2021-07-07 23:22:38 -0400

    nav hiding works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
-import { CertificateDashboardComponent } from './modules/certificate-dashboard/certificate-dashboard.component';
-import { LanIDDashboardComponent } from './modules/lan-iddashboard/lan-iddashboard.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CertificateDashboardComponent,
-    LanIDDashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
