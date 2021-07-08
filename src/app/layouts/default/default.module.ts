92522ba51887f90c2ae492c5a16b67457f23c48b
ankurkejriwal <kejriwal99@gmail.com>
2021-07-07 22:07:11 -0400

    Navbar,sidebar & footer work

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { UrlRedirectsComponent } from 'src/app/modules/url-redirects/url-redirects.component';
 import { HealthCheckComponent } from 'src/app/modules/health-check/health-check.component';
+
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import {MatSidenavModule} from '@angular/material/sidenav'
+import { MatDividerModule} from '@angular/material/divider'
 
 
 
@@ -23,6 +27,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     RouterModule,
     SharedModule,
+    MatSidenavModule,
+    MatDividerModule
   ]
 })
 export class DefaultModule { }
